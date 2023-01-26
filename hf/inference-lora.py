1c236012be1e8a2ae3023e530d15743b9d2a1977
Sangsu <ggb04110@naver.com>
2023-01-26 10:03:14 +0000

    wandb_setting and fix validation

--- hf/inference-lora.py
+++ hf/inference-lora.py
@@ -1,11 +1,13 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
+pretrained_model = 'runwayml/stable-diffusion-v1-5'
 model_path = "models/emoji-model-lora"
-pipe = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
-pipe.unet.load_attn_procs(model_path)
-pipe.to("cuda")
 
+pipe = StableDiffusionPipeline.from_pretrained(pretrained_model, torch_dtype=torch.float16)
+pipe.unet.load_attn_procs(model_path)
+
+pipe.to("cuda")
 prompt = "Cute rabbit wearing a blue hat and eating a carrot"
-image = pipe(prompt=prompt, guidance_scale=7.5).images[0]
+image = pipe(prompt=prompt, guidance_scale=15.0).images[0]
 image.save(f"outputs/{prompt[:10]}3.png")
