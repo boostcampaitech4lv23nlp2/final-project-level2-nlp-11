1eff4a0767e7bb96f22d65904f166dd9a1d910ea
soypabloo <kate49143@gmail.com>
2023-01-27 01:52:23 +0000

    fix inference.py

--- hf/inference-lora.py
+++ hf/inference-lora.py
@@ -1,11 +1,18 @@
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, UNet2DConditionModel
 import torch
+from accelerate import Accelerator
 
-model_path = "models/emoji-model-lora"
-pipe = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
-pipe.unet.load_attn_procs(model_path)
+torch.cuda.empty_cache()
+model_path = "stabilityai/stable-diffusion-2-1-base"
+ckpt_path = "models/2-1-base-test-inference/checkpoint-15000"
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_path,
+    revision=None,
+    torch_dtype=torch.float16,
+)
+pipe.unet.load_attn_procs(ckpt_path)
 pipe.to("cuda")
 
 prompt = "Cute rabbit wearing a blue hat and eating a carrot"
 image = pipe(prompt=prompt, guidance_scale=7.5).images[0]
-image.save(f"outputs/{prompt[:10]}3.png")
+image.save(f"outputs/{prompt[:10]}10.png")
