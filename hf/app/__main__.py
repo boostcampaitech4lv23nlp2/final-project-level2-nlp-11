5fe1c15ebcfc551a308a24a6517fcbeabadd5944
Sangsu <ggb04110@naver.com>
2023-01-28 08:26:35 +0000

    :art: append backend main

--- hf/app/__main__.py
+++ hf/app/__main__.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=6026, reload=True)
