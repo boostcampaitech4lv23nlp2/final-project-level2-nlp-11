81c1299348297651c7239897650c5c6a8b96d418
Sangsu <ggb04110@naver.com>
2023-01-28 08:25:15 +0000

    :art: fix front connect backend

--- hf/app/frontend.py
+++ hf/app/frontend.py
@@ -1,18 +1,45 @@
 import streamlit as st
+import io
+import base64
+import requests
 import streamlit_nested_layout
 from PIL import Image
 
 st.set_page_config(page_title="Text-to-Emoji",layout="wide")
+
 
 def main() :
     left, right = st.columns([4, 1])
 
     if "submit" not in st.session_state:
         st.session_state["submit"] = False
+    if "prompt" not in st.session_state :
+        st.session_state['prompt'] = ""
+    if "image_list" not in st.session_state :
+        st.session_state['image_list'] = []
     if "output_size" not in st.session_state :
-        st.session_state["output_size"] = "128"
+        st.session_state["output_size"] = 256
     if "num_inference" not in st.session_state :
-        st.session_state["num_inference"] = "4"
+        st.session_state["num_inference"] = None
+    if "guidance_scale" not in st.session_state : 
+        st.session_state['guidance_scale'] = None
+    if "inference_step" not in st.session_state : 
+        st.session_state['inference_step'] = None
+    if "save_prameter" not in st.session_state : 
+        st.session_state['save_prameter'] = {}
+    
+    #512 512 columns 4
+    #256 256 columns 4
+    #128 128 columns 8
+    #64 64 columns 16
+    image_reize_dict = {
+        "64" : 16,
+        "128" : 8,
+        "256" : 4,
+        "512" : 4,
+        }
+    
+    size_column = image_reize_dict[''.join(str(st.session_state.output_size))]
 
     with left :
         st.subheader("Text-to-Emoji")
@@ -22,43 +49,53 @@ def main() :
 
             with col1:
                 st.text_input(
-                    "",
+                    "a cute buddy rabbit",
                     placeholder="a cute buddy rabbit",
-                    key="input",
+                    key="prompt",
                     label_visibility="collapsed",
                 )
             with col2:
                 submit = st.form_submit_button(label="submit")
-
+                if submit :
+                    st.session_state.submit = True
+        
         if st.session_state.submit :
-            print(st.session_state.output_size)
-
-            #512 512 columns 4
-            #256 256 columns 4
-            #128 128 columns 8
-            #64 64 columns 16
-            image_reize_dict = {
-                "64" : 16,
-                "128" : 8,
-                "256" : 4,
-                "512" : 4,
-            }
-
-            image1 = Image.open('../outputs/Cute rabbi3.png')
-
-            img_size = int(st.session_state.output_size)
-            size_column = image_reize_dict[st.session_state.output_size]
-            num_inference = st.session_state.num_inference
-
-            image1 = image1.resize((img_size,img_size))
+            data = {
+                "prompt": st.session_state.prompt,
+                "guidance_scale":  st.session_state.guidance_scale,
+                "num_inference":  st.session_state.num_inference,
+                "inference_step":  st.session_state.inference_step,
+                "resize":  st.session_state.output_size
+                }
             
-            image_list = [image1] * 20
-            image_list = image_list[:num_inference]
+            print(data)
+            st.session_state.save_parameter = data
+
+            response = requests.post( "http://localhost:6026/submit",json= data)
+
+            image_byte_list = response.json()['products']['image_list']
+            decode_image_list = [Image.open(io.BytesIO(base64.b64decode(image))) for image in image_byte_list ]
+            # image1 = Image.open('../outputs/Cute rabbi3.png')
+            size_column = image_reize_dict[''.join(str(st.session_state.output_size))]
+            st.session_state['image_list'] = decode_image_list
 
             cols = st.columns(size_column)
-            
-            for idx , image in enumerate(image_list) :
+
+            for idx , image in enumerate(st.session_state['image_list']) :
                 cols[idx%size_column].image(image, use_column_width="auto")
+
+            st.session_state.submit = False
+
+        else :
+            if st.session_state['image_list'] :
+                output_size = st.session_state.save_parameter['resize']
+                size_column = image_reize_dict[''.join(str(output_size))]
+                cols = st.columns(size_column)
+
+                for idx , image in enumerate(st.session_state['image_list']) :
+                    cols[idx%size_column].image(image, use_column_width="auto")
+
+
             
     with right :
 
@@ -66,7 +103,6 @@ def main() :
             "output_size",
             ("128","256","512")
         )
-        st.session_state['output_size'] = output_option
         st.write(output_option)
         st.markdown("##")
 
@@ -79,23 +115,21 @@ def main() :
         st.write(model_option)
         st.markdown("##")
 
-        num_inference = st.slider("",0,12,4,label_visibility="hidden")
-        st.session_state['num_inference'] = num_inference
+        num_inference = st.slider("",0,8,4,label_visibility="hidden")
         st.write(f'num_inference : {num_inference}')
         st.markdown("##")
 
-        cfg_value = st.slider("",0, 100, 7, label_visibility="hidden")
-        st.write(f'cfg_scale : {cfg_value}')
+        guidance_scale = st.slider("",0, 100, 20, label_visibility="hidden")
+        st.write(f'cfg_scale : {guidance_scale}')
         st.markdown("##")
 
         inference_step = st.slider("",0, 100, 30, label_visibility="hidden")
         st.write(f'inference_step : {inference_step}')
-    
-    
-    if submit :
-        st.session_state.submit = True
-        st.experimental_rerun()
-    
+
+        st.session_state['output_size'] = int(output_option)
+        st.session_state['num_inference'] = int(num_inference)
+        st.session_state['guidance_scale'] = int(guidance_scale)
+        st.session_state['inference_step'] = int(inference_step)
     
 if __name__ == "__main__" :
     main()
